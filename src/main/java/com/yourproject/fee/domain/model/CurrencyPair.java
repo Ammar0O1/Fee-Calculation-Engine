549729979a66c154dfcd3ce9779b4255a2d4b6ed
Blnd-Ismail <blndismahil10@gmail.com>
2026-04-15 15:21:37 +0300

    Added enums and rounding for currency.

---
+++ src/main/java/com/yourproject/fee/domain/model/CurrencyPair.java
@@ -0,0 +1,26 @@
+package com.yourproject.fee.domain.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import java.util.Objects;
+
+@Getter
+@RequiredArgsConstructor
+//checks currency and compares them we have full and partial
+public class CurrencyPair {
+    private final Currency sourceCurrency;
+    private final Currency destinationCurrency;
+
+    public boolean isExactMatch(Currency sourceCurrency,Currency destinationCurrency){
+        return Objects.equals(this.sourceCurrency,sourceCurrency) && Objects.equals(this.destinationCurrency,destinationCurrency);
+    }
+    public boolean isPartialDestinationMatch(Currency destinationCurrency){
+        return  Objects.equals(this.destinationCurrency, destinationCurrency) && this.sourceCurrency==null ;
+    }
+    public boolean isPartialSourceMatch(Currency sourceCurrency){
+        return Objects.equals(this.sourceCurrency, sourceCurrency) && this.destinationCurrency==null;
+    }
+    public boolean isWildcard(){
+        return this.sourceCurrency==null && this.destinationCurrency==null;
+    }
+}
