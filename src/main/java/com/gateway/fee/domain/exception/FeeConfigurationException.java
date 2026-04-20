e0d6cf85eea525ed266b097eab5f08ebf6021087
Blnd-Ismail <blndismahil10@gmail.com>
2026-04-20 14:18:04 +0300

    Added FeeConfigurationException and .gitkeep deleted

---
+++ src/main/java/com/gateway/fee/domain/exception/FeeConfigurationException.java
@@ -0,0 +1,7 @@
+package com.gateway.fee.domain.exception;
+
+public class FeeConfigurationException extends RuntimeException {
+    public FeeConfigurationException(String message) {
+        super(message);
+    }
+}
