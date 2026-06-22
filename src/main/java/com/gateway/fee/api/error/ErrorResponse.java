a1f14ab697c278a9f835223f66c66da883a37e46
Hardy1111 <hardyhakim45@gmail.com>
2026-06-22 10:21:43 +0300

    Add ErrorResponse.java to api/error

---
+++ src/main/java/com/gateway/fee/api/error/ErrorResponse.java
@@ -0,0 +1,6 @@
+package com.gateway.fee.api.error;
+
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(int status, String error, String message, LocalDateTime timestamp, String path){ }
