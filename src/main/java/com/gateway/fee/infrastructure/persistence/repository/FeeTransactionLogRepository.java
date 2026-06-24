a525d4da4c8340d92e81e84ffb1f12e41145c676
Hardy1111 <hardyhakim45@gmail.com>
2026-06-24 15:28:40 +0300

    add FeeHistoryResponse ,  FeeHistoryService (unfinished) , also modfided FeeTransactionLogRepository : adding a querry method so that i can use in the FeeHistoryService

--- src/main/java/com/gateway/fee/infrastructure/persistence/repository/FeeTransactionLogRepository.java
+++ src/main/java/com/gateway/fee/infrastructure/persistence/repository/FeeTransactionLogRepository.java
@@ -1,18 +1,42 @@
-<<<<<<<< HEAD:src/main/java/com/gateway/fee/infrastructure/persistence/FeeTransactionLogRepository.java
-package com.gateway.fee.infrastructure.persistence;
-========
 package com.gateway.fee.infrastructure.persistence.repository;
->>>>>>>> aecf952 (add DatabaseIntegrationTest extending AbstractPostgresIntegrationTest, organize repository folder):src/main/java/com/gateway/fee/infrastructure/persistence/repository/FeeTransactionLogRepository.java
 import com.gateway.fee.domain.model.TransactionType;
 import com.gateway.fee.infrastructure.persistence.entity.FeeTransactionLogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+import com.gateway.fee.domain.model.Currency;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 public interface FeeTransactionLogRepository extends JpaRepository<FeeTransactionLogEntity, UUID> {
 
     List<FeeTransactionLogEntity> findByTransactionId(String transactionId);
     List<FeeTransactionLogEntity> findBySenderUserIdAndCalculatedAtBetween(String senderUserId, LocalDateTime from, LocalDateTime to);
     List<FeeTransactionLogEntity> findByTransactionTypeAndCalculatedAtBetween(TransactionType transactionType, LocalDateTime from, LocalDateTime to);
+
+    @Query("SELECT log FROM FeeTransactionLogEntity log WHERE "
+            + "(:transactionId IS NULL OR log.transactionId = :transactionId) "
+            + " AND (:userId IS NULL OR log.senderUserId = :userId OR log.receiverUserId = :userId) "
+            + " AND (:transactionType IS NULL OR log.transactionType = :transactionType)"
+            + " AND (:sourceCurrency IS NULL OR log.sourceCurrency = :sourceCurrency)"
+            + " AND (:destinationCurrency IS NULL OR log.destinationCurrency = :destinationCurrency)"
+            + " AND (:dateFrom IS NULL OR log.calculatedAt >= :dateFrom)"
+            + " AND (:dateTo IS NULL OR log.calculatedAt <= :dateTo)")
+
+    Page<FeeTransactionLogEntity> findHistory(
+            @Param("transactionId") String transactionId,
+            @Param("userId") String userId,
+            @Param("transactionType") TransactionType transactionType,
+            @Param("sourceCurrency") Currency sourceCurrency,
+            @Param("destinationCurrency") Currency destinationCurrency,
+            @Param("dateFrom") LocalDateTime dateFrom,
+            @Param("dateTo") LocalDateTime dateTo,
+            Pageable pageable
+    );
 }
+
+
