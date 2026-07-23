f1ed42aa2d92b4e1ab1e60452c55106908429c16
Ammar0O1 <ammararkan@outlook.com>
2026-07-23 15:26:53 +0300

    test: clean up unused imports after credential externalization

--- src/test/java/com/gateway/fee/api/AbstractApiIntegrationTest.java
+++ src/test/java/com/gateway/fee/api/AbstractApiIntegrationTest.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -31,6 +29,7 @@ public abstract class AbstractApiIntegrationTest {
     void clearDatabase() {
         feeRuleRepository.deleteAll();
     }
+
     @AfterEach
     void cleanupAfter() {
         feeRuleRepository.deleteAll();
